e8f3d6428fbfc4cc374f5b699e7bc3bceba9abee
Leroy Kirui <delleroy04@gmail.com>
2025-07-20 11:26:56 +0300

    Testing

--- django-models/LibraryProject/relationship_app/Views/librarian_view.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render
-
-def is_librarian(user):
-    return hasattr(user, 'UserProfile') and user.UserProfile.role == 'Librarian'
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
