58a2816a80dfe4bf2f94fa488dde8ba3f818add9
Leroy Kirui <delleroy04@gmail.com>
2025-07-25 19:27:04 +0300

    Understanding how to build a robust and scalable web applications using Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,9 +4,9 @@ from .views import list_books, LibraryDetailView, LoginView, LogoutView, registe
 urlpatterns = [ 
     path('books/', views.list_books, name='list_books'),
     path('library/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('admin/', views.UserCreationForm.as_view(), name='admin_detail'),
-    path('librarian/', views.UserCreationForm.as_view(template_name='librarian_view.html'), name='librarian_detail'),
-    path('member/', views.UserCreationForm.as_view(), name='member_detail'),
+    path('admin/', views.admin_view.as_view(), name='admin_detail'),
+    path('librarian/', views.librarian_view.as_view(template_name='librarian_view.html'), name='librarian_detail'),
+    path('member/', views.member_view.as_view(), name='member_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
