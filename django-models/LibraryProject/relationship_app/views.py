0645faeab620f7391fde681841420cf4c3a979a1
Leroy Kirui <delleroy04@gmail.com>
2025-07-25 19:24:11 +0300

    Understanding how to build a robust and scalable web applications using Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -49,6 +49,28 @@ def register(request):
     return render(request, 'relationship_app/register.html', {'form': form})
 
 
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+
 
 
 
