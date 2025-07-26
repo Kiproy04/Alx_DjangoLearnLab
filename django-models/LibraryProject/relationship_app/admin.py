3aa3ff049cc0c103842ce96da5466941c05fc0d0
Leroy Kirui <delleroy04@gmail.com>
2025-07-26 15:41:49 +0300

    Understanding how to build a robust and scalable web applications using Django

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book, UserProfile, Author, Library, Librarian
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(UserProfile)
+admin.site.register(Author)
+admin.site.register(Library)
+admin.site.register(Librarian)
