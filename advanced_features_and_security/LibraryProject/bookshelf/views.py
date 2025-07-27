eb820fd04b2af7cfc9a47adf47c7d818c8262bef
Leroy Kirui <delleroy04@gmail.com>
2025-07-27 14:02:11 +0300

    Renamed and Testing

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
-@permission_required('bookshelf.can_view_book')
+@permission_required('bookshelf.can_view_book', raise_exception=True)
 def view_books(request):
    ...
 
