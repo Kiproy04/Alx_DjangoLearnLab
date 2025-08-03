f54af0f2478bff75ebeb10b3e17534bf98ea743a
Leroy Kirui <delleroy04@gmail.com>
2025-08-03 05:10:28 +0300

    Working with APIs

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
-    serializer_class = 'BookSerializer'
+    serializer_class = BookSerializer
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
