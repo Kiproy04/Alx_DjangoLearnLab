f1204107c6907268fe7f00018da539a67845993a
Leroy Kirui <delleroy04@gmail.com>
2025-08-11 14:42:46 +0300

    Testing

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
-
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics, permissions, filters
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 # Create your views here.
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # read-only for everyone
+
+class IsOwnerOrReadOnly(BasePermission):
+    """
+    Allow safe methods for everyone. For write methods, only allow the object's creator or staff.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        # obj.created_by may be null; check both conditions
+        return (hasattr(obj, 'created_by') and obj.created_by == request.user) or request.user.is_staff
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # attach the current user to created_by automatically
+        serializer.save(created_by=self.request.user)
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    # if you need to do something special when updating, override perform_update
+    def perform_update(self, serializer):
+        serializer.save() 
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+
