2af2ca44865722ffece93ee2c7a83658a77e3880
Kiproy04 <delleroy04@gmail.com>
2025-08-18 10:39:27 +0300

    Trying to pass test 1 develop an authentication logic

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.conf import settings
-
-User = get_user_model()
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -14,12 +12,12 @@ class Post(models.Model):
         return f"{self.author} - {self.title} - {self.content}"
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
-    bio = models.TextField(blank=True)
-    avatar = models.ImageField(upload_to="profiles/", blank=True, null=True)
+# class Profile(models.Model):
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
+#     bio = models.TextField(blank=True)
+#     avatar = models.ImageField(upload_to="profiles/", blank=True, null=True)
 
-    def __str__(self):
-        return f"{self.user.username}'s profile"
+#     def __str__(self):
+#         return f"{self.user.username}'s profile"
 
 
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import render, redirect
 from .forms import RegistrationForm, UserUpdateForm, ProfileUpdateForm
 
 class UserLoginView(LoginView):
