4bd4c9253c5bc806ed8e093515a57df89af51f41
Kiproy04 <delleroy04@gmail.com>
2025-08-19 16:41:46 +0300

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager(blank=True)
 
     def __str__(self):
         return f"{self.author} - {self.title} - {self.content}"
@@ -32,6 +34,6 @@ class Comment(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="tag")
 
-    def
+    def __str__(self):
+        return self.name
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserLoginView, UserLogoutView, register, profile, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CommentCreateView, CommentUpdateView, CommentDeleteView, posts_by_tag, search_posts
+from .views import UserLoginView, UserLogoutView, register, profile, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CommentCreateView, CommentUpdateView, CommentDeleteView
 from . import views
 urlpatterns = [
     path("login/", UserLoginView.as_view(), name="login"),
@@ -14,7 +14,6 @@ urlpatterns = [
     path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="comment-create"),
     path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
-    path("tags/<str:tag_name>/", views.posts_by_tag, name="posts_by_tag"),
     path('search/', views.search_posts, name='search_posts'),
-    path("tags/<slug:tag_slug>/", views.PostByTagListView.as_view(), name="posts_by_tag"),
+    path("tags/<slug:tag_slug>/", views.PostByTagListView.as_view(), name="post_list_by_tag"),
 ]
